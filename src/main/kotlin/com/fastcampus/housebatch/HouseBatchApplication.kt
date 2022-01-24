db4435cb2b6ffbff1910f9a4c48b361d235d156f
hoon <moradulee@gmail.com>
2022-01-24 23:44:00 +0900

    first commit

---
+++ src/main/kotlin/com/fastcampus/housebatch/HouseBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.fastcampus.housebatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HouseBatchApplication
+
+fun main(args: Array<String>) {
+	runApplication<HouseBatchApplication>(*args)
+}
